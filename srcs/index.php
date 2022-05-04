b7c2afe78c2c76569da0d56d63f43d7e8dfffe96
Omar Alsakka <djomrofficial@gmail.com>
2022-05-04 16:08:04 +0300

    created media icons and HTML file

---
+++ srcs/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<title>Camagru Signup</title>
+
+	<style>
+		
+		/* default size for all elements */
+		*{
+			margin:0;
+			padding:0;
+			box-sizing: border-box;
+		}
+		.instagram-container{
+			width: 100%;
+			max-width: 350px;
+			margin: auto;
+			/* height: 200px;
+			background-color: yellow; */
+		}
+	</style>
+</head>
+<body>
+
+	<div class="instagram-container">
+		<div class="instagram-logo">
+			<img src="" alt="brand logo">
+		</div>
+	</div>
+
+</body>
+</html>
