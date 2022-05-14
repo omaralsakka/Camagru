0f9dcef2e3784fa0d27588109c3c2d50bfc2514b
Omar Alsakka <djomrofficial@gmail.com>
2022-05-14 14:43:48 +0300

    we access filter in php file

--- camera-app/image.php
+++ camera-app/image.php
@@ -1,31 +1,10 @@
 <?php
-	// //get current directory
-	// $working_dir = getcwd();
-	
-	// //get image directory
-	// $img_dir = "../media/filters";
-	
-	// //change current directory to image directory
-	// chdir($img_dir);
-	
-	// //using glob() function get images 
-	// $files = glob("*.{jpg,jpeg,png,gif,JPG,JPEG,PNG,GIF}", GLOB_BRACE );
-	
-	// //again change the directory to working directory
-	// chdir($working_dir);
 
-	//iterate over image files
+    if (isset($_POST['filterNumber'])){
+        $filter = '../media/filters/'.$_POST['filterNumber'];
+        echo $filter;
+    }
 
-    // if (isset($_GET['filter'])){
-    //     echo "<h1>Success</h1>";
-    // }
-    
-    // if (isset($_POST['filter']))
-    // {
-    //     echo $_POST['filter'];
-    //     echo "<h1>Success</h1>";
-    // }
-    
     if (isset($_POST['base64image'])){
         echo "<img src=".$_POST['base64image']."> <h1>".$_POST['filter']."</h1>";
     }
