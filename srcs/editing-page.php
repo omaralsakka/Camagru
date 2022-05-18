f60d9e3b8c2adab534c8e120d8cd05f4c6560ed6
Omar Alsakka <djomrofficial@gmail.com>
2022-05-18 13:08:12 +0300

    editing page styling done

--- srcs/editing-page.php
+++ srcs/editing-page.php
@@ -13,44 +13,40 @@
     
     <div class="main-container">
         
-        <div class="filters-container">
-            <button class="filter"></button>
-            <button class="filter"></button>
-            <button class="filter"></button>
-            <button class="filter"></button>
-            <button class="filter"></button>
-            <button class="filter"></button>
-            <button class="filter"></button>
-            <button class="filter"></button>
-            <button class="filter"></button>
-        </div>
+        <?php include_once('../frontend/filtersContainer.html')?>
 
         <div class="view-image-container">
             <div class="display">
 
             </div>
             <button class="capture-button">
-
+                <img id="capture-icon" src="../media/icons/icons8-lense-64.png" alt="capture icon image">
             </button>
         </div>
 
         <div class="thumbnail-container">
-            <div class="thumbnail-image"></div>
-            <div class="thumbnail-image"></div>
-            <div class="thumbnail-image"></div>
-            <div class="thumbnail-image"></div>
-            <div class="thumbnail-image"></div>
-            <div class="thumbnail-image"></div>
-            <div class="thumbnail-image"></div>
-            <div class="thumbnail-image"></div>
-            <div class="thumbnail-image"></div>
+            <div class="thumbnail-image-container">
+                <img src="../media/html_image.png" alt="thumbnail image" class="thumbnail-image">
+            </div>
+            <div class="thumbnail-image-container">
+                <img src="../media/html_image.png" alt="thumbnail image" class="thumbnail-image">
+            </div>
+            <div class="thumbnail-image-container">
+                <img src="../media/html_image.png" alt="thumbnail image" class="thumbnail-image">
+            </div>
         </div>
     </div>
     <div class="footer">
         <div class="buttons-container">
-            <button class="button-option"></button>
-            <button class="button-option"></button>
-            <button class="button-option"></button>
+            <button class="button-option">
+                <img src="../media/icons/icons8-camera-100-outline.png" alt="" class="option-image">
+            </button>
+            <button class="button-option">
+                <img src="../media/icons/icons8-folder-100-outline.png" alt="" class="option-image">
+            </button>
+            <button class="button-option">
+                <img src="../media/icons/icons8-done-100.png" alt="" class="option-image">
+            </button>
         </div>
     </div>
 
