71fb3bcecb597d2c3e0b1d81df693275097d7706
Omar Alsakka <djomrofficial@gmail.com>
2022-05-18 11:53:38 +0300

    styled the basics for editing front-end

---
+++ srcs/editing-page.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include_once('../frontend/head.html')?>
+    <!-- <link rel="stylesheet" href="../style/style.css"> -->
+    <link rel="stylesheet" href="../style/editing-page.css">
+</head>
+<style>
+
+</style>
+<body>
+    <?php include_once('../frontend/navbar.html')?>
+    
+    <div class="main-container">
+        
+        <div class="filters-container">
+
+        </div>
+
+        <div class="view-image-container">
+
+        </div>
+
+        <div class="thumbnail-container">
+
+        </div>
+    </div>
+    <div class="footer">
+
+        <div class="buttons-container">
+            
+        </div>
+    
+    </div>
+
+</body>
+</html>
