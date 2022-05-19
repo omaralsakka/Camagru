287877d901722669d64106de5d9af54fd5706a57
Omar Alsakka <djomrofficial@gmail.com>
2022-05-19 11:24:14 +0300

    front end for home page done

--- srcs/home.php
+++ srcs/home.php
@@ -50,16 +50,135 @@ if(!isset($_SESSION['user_id'])){
 				<button class="action-button">
 					<img src="../media/icons/icons8-comment-64-outline.png" alt="" class="action-image">
 				</button>
+
+				<div class="username-container">
+					<h3 class="username">officialomr</h3>
+				</div>
 			</div>
 
 			<div class="comment-container">
-				<!-- <input class="comment-field" type="text"> -->
+				<input class="comment-field" type="text">
 			</div>
 
 			<div class="user-comments-container">
-
+				<div class="user-comment-info">
+					<h3 id="comment-username">officialomr</h3>
+					<h5 id="comment-date">14.05.2022</h5>
+				</div>
+				<hr id="comments-horizontal-line">
+				<div class="user-comment-text">
+					<h3 id="user-comment-text">Nice stuff!</h3>
+				</div>
 			</div>
 		</div>
+
+		<div class="item-container">
+			
+			<div class="picture-container">
+				<img src="../media/html_image.png" alt="" class="picture">
+			</div>
+			
+			<div class="action-container">
+				<button class="action-button">
+					<img src="../media/icons/icons8-heart-outline.png" alt="" class="action-image">
+				</button>
+
+				<button class="action-button">
+					<img src="../media/icons/icons8-comment-64-outline.png" alt="" class="action-image">
+				</button>
+
+				<div class="username-container">
+					<h3 class="username">officialomr</h3>
+				</div>
+			</div>
+
+			<div class="comment-container">
+				<input class="comment-field" type="text">
+			</div>
+
+			<div class="user-comments-container">
+				<div class="user-comment-info">
+					<h3 id="comment-username">officialomr</h3>
+					<h5 id="comment-date">14.05.2022</h5>
+				</div>
+				<hr id="comments-horizontal-line">
+				<div class="user-comment-text">
+					<h3 id="user-comment-text">Nice stuff!</h3>
+				</div>
+			</div>
+		</div>
+
+		<div class="item-container">
+			
+			<div class="picture-container">
+				<img src="../media/html_image.png" alt="" class="picture">
+			</div>
+			
+			<div class="action-container">
+				<button class="action-button">
+					<img src="../media/icons/icons8-heart-outline.png" alt="" class="action-image">
+				</button>
+
+				<button class="action-button">
+					<img src="../media/icons/icons8-comment-64-outline.png" alt="" class="action-image">
+				</button>
+
+				<div class="username-container">
+					<h3 class="username">officialomr</h3>
+				</div>
+			</div>
+
+			<div class="comment-container">
+				<input class="comment-field" type="text">
+			</div>
+
+			<div class="user-comments-container">
+				<div class="user-comment-info">
+					<h3 id="comment-username">officialomr</h3>
+					<h5 id="comment-date">14.05.2022</h5>
+				</div>
+				<hr id="comments-horizontal-line">
+				<div class="user-comment-text">
+					<h3 id="user-comment-text">Nice stuff!</h3>
+				</div>
+			</div>
+		</div>
+
+		<div class="item-container">
+			
+			<div class="picture-container">
+				<img src="../media/html_image.png" alt="" class="picture">
+			</div>
+			
+			<div class="action-container">
+				<button class="action-button">
+					<img src="../media/icons/icons8-heart-outline.png" alt="" class="action-image">
+				</button>
+
+				<button class="action-button">
+					<img src="../media/icons/icons8-comment-64-outline.png" alt="" class="action-image">
+				</button>
+
+				<div class="username-container">
+					<h3 class="username">officialomr</h3>
+				</div>
+			</div>
+
+			<div class="comment-container">
+				<input class="comment-field" type="text">
+			</div>
+
+			<div class="user-comments-container">
+				<div class="user-comment-info">
+					<h3 id="comment-username">officialomr</h3>
+					<h5 id="comment-date">14.05.2022</h5>
+				</div>
+				<hr id="comments-horizontal-line">
+				<div class="user-comment-text">
+					<h3 id="user-comment-text">Nice stuff!</h3>
+				</div>
+			</div>
+		</div>	
     </div>
 
 </body>
