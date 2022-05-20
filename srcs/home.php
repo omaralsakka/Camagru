5299509d5226fa4c2dcd9dde5de6bfaf588f110d
Omar Alsakka <djomrofficial@gmail.com>
2022-05-20 19:25:28 +0300

    now all images displays in home with it's own info

--- srcs/home.php
+++ srcs/home.php
@@ -4,8 +4,6 @@
 
 error_reporting(0);
 
-// including the connection to mysql database file.
-require_once('config.php');
 
 // starting session to pass through server the user data.
 session_start();
@@ -20,6 +18,11 @@ if(!isset($_SESSION['user_id'])){
     // save the user_id into a variable
     $userId = $_SESSION['user_id'];
 }
+
+$dbh = new PDO("mysql:host=localhost;dbname=camagru_website", "root", "123456");
+$username = $_SESSION['username'];
+$stat = $dbh->prepare("SELECT * FROM user_images");
+$stat->execute();
 
 ?>
 
@@ -36,150 +39,63 @@ if(!isset($_SESSION['user_id'])){
 	<?php include_once("../frontend/navbar.html");?>
 
     <div class="main-container">
-		<div class="item-container">
-			
-			<div class="picture-container">
-				<img src="../media/html_image.png" alt="" class="picture">
-			</div>
-			
-			<div class="action-container">
-				<button class="action-button">
-					<img src="../media/icons/icons8-heart-outline.png" alt="" class="action-image">
-				</button>
+		<?php
+	        while($row = $stat->fetch()){
+				$username = $row['username'];
+				$date = date('Y.m.d', strtotime($row['date']));
+				$type = $row['type'];
+				$content = base64_encode($row['content']);
+				echo 	'<div class="item-container">
+							<div class="picture-container">
+								<img src="'.$type.$content.'" alt="" class="picture">
+							</div>
+							<div class="action-container">
+								<button class="action-button">
+									<img src="../media/icons/icons8-heart-outline.png" alt="" class="action-image">
+								</button>
+								<button id="display-comment" class="action-button" onclick="displayCommentInput()">
+									<img src="../media/icons/icons8-comment-64-outline.png" alt="" class="action-image">
+								</button>
 
-				<button class="action-button">
-					<img src="../media/icons/icons8-comment-64-outline.png" alt="" class="action-image">
-				</button>
+								<div class="username-container">
+									<h3 class="username">'.$username.'</h3>
+								</div>
 
-				<div class="username-container">
-					<h3 class="username">officialomr</h3>
-				</div>
-			</div>
-
-			<div class="comment-container">
-				<input class="comment-field" type="text">
-			</div>
-
-			<div class="user-comments-container">
-				<div class="user-comment-info">
-					<h3 id="comment-username">officialomr</h3>
-					<h5 id="comment-date">14.05.2022</h5>
-				</div>
-				<hr id="comments-horizontal-line">
-				<div class="user-comment-text">
-					<h3 id="user-comment-text">Nice stuff!</h3>
-				</div>
-			</div>
-		</div>
-
-		<div class="item-container">
-			
-			<div class="picture-container">
-				<img src="../media/html_image.png" alt="" class="picture">
-			</div>
-			
-			<div class="action-container">
-				<button class="action-button">
-					<img src="../media/icons/icons8-heart-outline.png" alt="" class="action-image">
-				</button>
-
-				<button class="action-button">
-					<img src="../media/icons/icons8-comment-64-outline.png" alt="" class="action-image">
-				</button>
-
-				<div class="username-container">
-					<h3 class="username">officialomr</h3>
-				</div>
-			</div>
-
-			<div class="comment-container">
-				<input class="comment-field" type="text">
-			</div>
-
-			<div class="user-comments-container">
-				<div class="user-comment-info">
-					<h3 id="comment-username">officialomr</h3>
-					<h5 id="comment-date">14.05.2022</h5>
-				</div>
-				<hr id="comments-horizontal-line">
-				<div class="user-comment-text">
-					<h3 id="user-comment-text">Nice stuff!</h3>
-				</div>
-			</div>
-		</div>
-
-		<div class="item-container">
-			
-			<div class="picture-container">
-				<img src="../media/html_image.png" alt="" class="picture">
-			</div>
-			
-			<div class="action-container">
-				<button class="action-button">
-					<img src="../media/icons/icons8-heart-outline.png" alt="" class="action-image">
-				</button>
-
-				<button class="action-button">
-					<img src="../media/icons/icons8-comment-64-outline.png" alt="" class="action-image">
-				</button>
-
-				<div class="username-container">
-					<h3 class="username">officialomr</h3>
-				</div>
-			</div>
-
-			<div class="comment-container">
-				<input class="comment-field" type="text">
-			</div>
-
-			<div class="user-comments-container">
-				<div class="user-comment-info">
-					<h3 id="comment-username">officialomr</h3>
-					<h5 id="comment-date">14.05.2022</h5>
-				</div>
-				<hr id="comments-horizontal-line">
-				<div class="user-comment-text">
-					<h3 id="user-comment-text">Nice stuff!</h3>
-				</div>
-			</div>
-		</div>
-
-		<div class="item-container">
-			
-			<div class="picture-container">
-				<img src="../media/html_image.png" alt="" class="picture">
-			</div>
-			
-			<div class="action-container">
-				<button class="action-button">
-					<img src="../media/icons/icons8-heart-outline.png" alt="" class="action-image">
-				</button>
-
-				<button class="action-button">
-					<img src="../media/icons/icons8-comment-64-outline.png" alt="" class="action-image">
-				</button>
-
-				<div class="username-container">
-					<h3 class="username">officialomr</h3>
-				</div>
-			</div>
-
-			<div class="comment-container">
-				<input class="comment-field" type="text">
-			</div>
-
-			<div class="user-comments-container">
-				<div class="user-comment-info">
-					<h3 id="comment-username">officialomr</h3>
-					<h5 id="comment-date">14.05.2022</h5>
-				</div>
-				<hr id="comments-horizontal-line">
-				<div class="user-comment-text">
-					<h3 id="user-comment-text">Nice stuff!</h3>
-				</div>
-			</div>
-		</div>	
+							</div>
+							<div class="picture-date">
+								<div id="date">'.$date.'</div>
+							</div>
+							<div class="comment-container">
+								<input class="comment-field" type="text">
+							</div>
+							<div class="user-comments-container">
+								<div class="user-comment-info">
+									<h3 id="comment-username">officialomr</h3>
+									<h5 id="comment-date">14.05.2022</h5>
+								</div>
+								<hr id="comments-horizontal-line">
+								<div class="user-comment-text">
+									<h3 id="user-comment-text">Nice stuff!</h3>
+								</div>
+							</div>
+						</div>';
+			}
+		?>
     </div>
 
 </body>
+<script>
+	let commentDisplayButton = document.getElementById('display-comment');
+	let commentField = document.querySelector('.comment-container');
+	
+	commentDisplayButton.addEventListener('click', function (){
+		if (commentField.style.display == "flex"){
+			commentField.style.display = "none";
+		} else {
+			commentField.style.display = "flex";
+		}
+	});
+
+
+</script>
 </html>
