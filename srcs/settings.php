bfaf629ff05194c673fe80956521b87660101411
Omar Alsakka <djomrofficial@gmail.com>
2022-05-24 10:19:35 +0300

    settings page frontend done

--- srcs/settings.php
+++ srcs/settings.php
@@ -9,19 +9,40 @@
 </head>
 <body>
     <?php include_once('../frontend/navbar.html')?>
+    
     <div class="main-container">
         <div class="settings-container">
-            <button class="change-name-btn">Change Fullname</button>
-            <button class="change-pass-btn">Change password</button>
-            <div class="notification-container">
+            <div class="setting-container checkbox">
+                <img class="setting-img" src="../media/icons/icons8-notification-96.png" alt="">
                 <label for="myCheck">Email notifications:</label> 
                 <input type="checkbox" id="myCheck" onclick="emailNotification()">
             </div>
-            <button class="delete-account-btn">Delete account</button>
+            <hr>
+            <div class="setting-container">
+                <img class="setting-img" src="../media/icons/icons8-change-name.png" alt="">
+                <button class="change-name-btn">Change full name</button>
+            </div>
+            <hr>
+            <div class="setting-container">
+                <img class="setting-img" src="../media/icons/icons8-change-email.png" alt="">
+                <button class="change-email-btn">Change email</button>
+            </div>
+            <hr>
+            <div class="setting-container">
+                <img class="setting-img" src="../media/icons/icons8-change-pass.png" alt="">
+                <button class="change-pass-btn">Change password</button>
+            </div>
+            <hr>
+            <div class="setting-container">
+                <img class="setting-img" src="../media/icons/icons8-remove-user-64.png" alt="">
+                <button class="delete-account-btn">Delete account</button>
+            </div>
         </div>
     </div>
+
 </body>
 <script>
+
     function emailNotification() {
         var checkBox = document.getElementById("myCheck");
         var text = document.getElementById("text");
